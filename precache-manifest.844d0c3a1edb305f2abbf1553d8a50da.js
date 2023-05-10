cef4ddd44d839e3c7b601713a3e91e70dbe25017
Aakash Mishra <aakashmishra.281@gmail.com>
2023-05-10 17:42:16 +0530

    Updates

---
+++ precache-manifest.844d0c3a1edb305f2abbf1553d8a50da.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb99e39e26de52b7d36e79cb29ce391",
+    "url": "/react-Login/index.html"
+  },
+  {
+    "revision": "aa323704f5555e42e1d1",
+    "url": "/react-Login/static/js/2.b609f7ea.chunk.js"
+  },
+  {
+    "revision": "ff3e54c6e406fa3cdbbb",
+    "url": "/react-Login/static/js/main.27974e2c.chunk.js"
+  },
+  {
+    "revision": "2bb7e6907cd2bbb31dac",
+    "url": "/react-Login/static/js/runtime-main.68f02968.js"
+  }
+]);
